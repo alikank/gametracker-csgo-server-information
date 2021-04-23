33100d5735e79da459f767ac664c2ca9cad8d4f9
Alikan <76900442+alikank@users.noreply.github.com>
2021-04-23 19:56:00 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+ $ip = "45.10.56.39";     //sadece burayı düzenleyin ,IP adresinizi girin. (enter ip address) (only edit here)
+
+echo verial("https://cache.gametracker.com/components/html0/?host=$ip:27015");
+
+
+ function verial($giris)
+ {
+
+ $c = stream_context_create(
+    array(
+        "http" => array(
+            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+        )
+    )
+);
+
+$giris = file_get_contents($giris, 0, $c);
+
+
+  preg_match_all('@<div class="info_line_right">(.*?)</div>@si',$giris,$yaz2);
+  preg_match_all('@<div class="server_name">(.*?)</div>@si',$giris,$yaz3);
+  preg_match_all('@<div class="item_float_right">(.*?)</div>@si',$giris,$yaz1);
+  
+  
+//sunucu ismi   
+echo $yaz3[0][0];
+
+//oyuncu sayısı
+echo "Oyuncu Sayısı :" . $yaz1[0][3] . "<br>"; 
+
+//gt rank
+echo $yaz1[0][4];
+
+//gt linki
+echo $yaz1[0][2];
+
+//açık harita
+echo $yaz2[0][0];
+
+  
+}
+?>
